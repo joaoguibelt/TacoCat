25427ccb3f40138148d843e73bd8d9253d8a173a
joaoguibelt <78770894+joaoguibelt@users.noreply.github.com>
2021-04-20 21:49:08 -0300

    criando_a_base
    Criei a janela preta e o game loop

---
+++ tacocat/Janela.java
@@ -0,0 +1,21 @@
+
+package tacocat;
+
+import java.awt.Canvas;
+import javax.swing.JFrame;
+
+
+public class Janela extends Canvas{
+    
+    public Janela(int width, int height, String nome, Jogo jogo){
+        JFrame frame = new JFrame(nome);
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setSize(width, height);        
+        frame.add(jogo);
+        frame.setResizable(false);
+        jogo.start();
+    }
+}
---
+++ tacocat/Jogo.java
@@ -0,0 +1,101 @@
+
+package tacocat;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
+public class Jogo extends Canvas implements Runnable{
+   
+    
+    public static final int W = 490;
+    public static final int H = 600;
+    
+    private Thread thread;
+    private boolean rodando = false;
+    
+    public Jogo(){
+        Janela j = new Janela(W, H, "TacoCat", this);
+    }
+    
+    
+    public synchronized void start(){
+        thread = new Thread(this);
+        thread.start();
+        rodando = true;
+    }
+    
+     public synchronized void stop(){
+        try {
+            thread.join();
+            rodando = false;
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Jogo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+    @Override
+    public void run() {
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+        while(rodando){
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while(delta >= 1){
+                tick();
+                delta--;
+            }
+            if(rodando){
+                render();
+            }
+            frames++;
+            if(System.currentTimeMillis() - timer > 1000){
+                timer += 1000;
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+        }
+        stop();
+    }
+
+    private void tick() {
+        
+    }
+
+    private void render() {
+        BufferStrategy bs = this.getBufferStrategy();
+        if(bs == null){
+            this.createBufferStrategy(3);
+            return;
+        }
+        
+        Graphics g = bs.getDrawGraphics();
+        
+        g.setColor(Color.orange);
+        g.fillRect(0, 0, W, H);
+        
+        
+        g.dispose();
+        bs.show();
+    }
+    
+ 
+    
+    
+   
+
+ 
+    
+    
+}
