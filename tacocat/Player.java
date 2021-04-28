588303a19012b95263628e03ff2d06e9fa8a3f89
joaoguibelt <78770894+joaoguibelt@users.noreply.github.com>
2021-04-28 11:02:50 -0300

    Codigo_Com_Erro

--- tacocat/Player.java
+++ tacocat/Player.java
@@ -2,15 +2,35 @@ package tacocat;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 
 
 public class Player extends GameObject{
     //Classe player vai ser o gato por enquanto só to testando as possibilidades
+
+           
+    BufferedImage imageParado;
+   // BufferedImage imageBatendo = null;
     
     Player(int x, int y){
         super(x, y, ID.Player);
       
-  
+        
+        try {
+            this.imageParado = ImageIO.read(getClass().getResource("/tacocat/Sprites/tacocat_gato_parado.png"));
+        } catch (IOException ex) {
+            Logger.getLogger(Player.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       // try {
+          //  this.imageBatendo = ImageIO.read(new File("tacocat gato batendo.png"));
+       // } catch (IOException ex) {
+         //   Logger.getLogger(Player.class.getName()).log(Level.SEVERE, null, ex);
+       // }
     }
     
     
@@ -22,8 +42,11 @@ public class Player extends GameObject{
 
     @Override
     public void render(Graphics g) {
-        g.setColor(Color.red);
-        g.fillRect(this.getX(), this.getY(), 50, 100);
+        
+        g.drawImage(imageParado, this.getX(), this.getY(), null);
+        
+        //g.setColor(Color.red);
+       // g.fillRect(this.getX(), this.getY(), 50, 100);
     }
 
     @Override
